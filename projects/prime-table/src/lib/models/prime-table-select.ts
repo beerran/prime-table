ca8f879eba62bc0b5bc24220a3f5c7bc9dd23b6e
Emil Larsson <emil.larsson@seb.se>
2018-11-06 17:14:53 +0100

    fix(select filtering): filter select in column
    fixed filtering of values in select box, added placeholder prop to PrimeTableSelect

--- projects/prime-table/src/lib/models/prime-table-select.ts
+++ projects/prime-table/src/lib/models/prime-table-select.ts
@@ -4,6 +4,7 @@ export class PrimeTableSelect {
     constructor(
         public label: string,
         public values: any[],
+        public placeholder = 'Select',
         public change: (event, row, col: PrimeTableColumn<any>) => any = null
     ) { }
 }
