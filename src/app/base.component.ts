5c5df881e290fc6207e9b8f58b9948505b9030af
Emil Larsson <larsson.emil@hotmail.com>
2018-11-06 21:34:22 +0100

    fix(fix base component): fix base component

--- src/app/base.component.ts
+++ src/app/base.component.ts
@@ -4,8 +4,8 @@ import { SnotifyService } from 'ng-snotify';
 export abstract class BaseComponent {
     constructor(protected snotify: SnotifyService) { }
     abstract infoText: string;
-    onAdd = (event: any, snotify: SnotifyService) => SharedStuff.onAdd(event, this.snotify);
-    onEdit = (event: any, snotify: SnotifyService) => SharedStuff.onEdit(event, this.snotify);
-    onArchive = (event: any, snotify: SnotifyService) => SharedStuff.onAdd(event, this.snotify);
-    onSelected = (event: {id: number}[], snotify: SnotifyService) => SharedStuff.onSelected(event, this.snotify);
+    onAdd = (event: any) => SharedStuff.onAdd(event, this.snotify);
+    onEdit = (event: any) => SharedStuff.onEdit(event, this.snotify);
+    onArchive = (event: any) => SharedStuff.onAdd(event, this.snotify);
+    onSelected = (event: {id: number}[]) => SharedStuff.onSelected(event, this.snotify);
 }
